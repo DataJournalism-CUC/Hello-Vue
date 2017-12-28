3cda6fbf51e150a46a83d2c920e2ade324dfca6b
DataJournalism-CUC <datajournalism_CUC@126.com>
2017-12-28 17:20:01 +0800

    达到index网页

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const Koa = require("koa");
 
 const app = new Koa();
 
+
+let staticFiles = require("./static-file");
+app.use(staticFiles("/static",__dirname + "/static"));
+
 app.use(async (ctx, next) => {
-    await next();
-    ctx.response.type = "text/html";
-    ctx.response.body = "hello-vue"
-})
+    ctx.response.redirect("/static/index.html");
+});
 
 app.listen(3000);
 console.log("app started at port 3000....")
