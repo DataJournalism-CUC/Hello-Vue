1da08dd1f83d32f49de65882e304347928bada8f
DataJournalism-CUC <datajournalism_CUC@126.com>
2017-12-28 17:05:54 +0800

    https://github.com/DataJournalism-CUC/Hello-Vue.git

---
+++ app.js
@@ -0,0 +1,12 @@
+const Koa = require("koa");
+
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+    await next();
+    ctx.response.type = "text/html";
+    ctx.response.body = "hello-vue"
+})
+
+app.listen(3000);
+console.log("app started at port 3000....")
